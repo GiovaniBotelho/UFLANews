d078df87b1932f955bdc0ae2db98b2b537ac53e7
Mauricio Vieira <mauriciomvdr@hotmail.com>
2019-10-30 19:40:12 -0300

    Componentes adicionados

--- src/components/presentational/Login/Login.js
+++ src/components/presentational/Login/Login.js
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, ActivityIndicator} from 'react-native';
+import {Text, Image} from 'react-native';
 import styled from 'styled-components';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import LinearGradient from 'react-native-linear-gradient';
+
+/* Images */
+import Logo from '../../../assets/header-login.png';
+import Mapa from '../../../assets/mapView.jpeg';
 
 const Login = props => {
-  const [nome, setNome] = useState('GG');
+  const [nome, setNome] = useState('');
+  const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(true);
 
   console.log(props);
   handleLogin = () => {
-    console.log(loading);
     setLoading(!loading);
 
     // props.navigation.navigate('Register');
@@ -16,46 +22,115 @@ const Login = props => {
 
   handleChangeText = nome => {
     setNome(nome);
-    console.log(nome);
   };
 
   return (
-    <View>
-      <StyledContainer>
-        <StyledInputText
-          placeholder={'email'}
-          value={nome}
-          onChangeText={texto => handleChangeText(texto)}
+    <>
+      <FormContainer colors={['#93cae8', '#c4cccf']}>
+        <Image
+          style={{width: '100%', height: '50px', paddingTop: 0, marginTop: 0}}
+          source={Mapa}
+          resizeMode={'contain'}
         />
-        <StyledInputText2 placeholder={'senha'} />
-      </StyledContainer>
-      <Button title={nome} onPress={() => handleLogin()}>
-        Teste
-      </Button>
-      <ActivityIndicator color={'green'} size={'large'} animating={!loading} />
-    </View>
+        <Image style={{width: '100%'}} source={Logo} resizeMode={'contain'} />
+        <StyledView>
+          <FormRow>
+            <Input>
+              <StyledIcon>
+                <Icon name="envelope" size={25} color="#000000" />
+              </StyledIcon>
+              <StyledInputText
+                placeholder={'E-mail'}
+                value={nome}
+                onChangeText={texto => handleChangeText(texto)}
+              />
+            </Input>
+            <Input>
+              <StyledIcon>
+                <Icon name="lock" size={25} color="#000000" />
+              </StyledIcon>
+              <StyledInputText placeholder={'Senha'} />
+            </Input>
+          </FormRow>
+          <StyledTextTouchableOpacity>
+            <StyledText textDecorationLine={'underline'}>
+              Esqueci a senha
+            </StyledText>
+          </StyledTextTouchableOpacity>
+          <StyledTouchableOpacity>
+            <Text>Login</Text>
+          </StyledTouchableOpacity>
+          <StyledTouchableOpacity>
+            <Text>Cadastrar</Text>
+          </StyledTouchableOpacity>
+        </StyledView>
+      </FormContainer>
+    </>
   );
 };
 
 /* Componentes estilizados aqui */
+const FormContainer = styled(LinearGradient)`
+  flex: 1;
+  justify-content: center;
+`;
+const StyledImages = styled.View`
+  width: 100%;
+  align-self: flex-start;
+`;
+const StyledView = styled.View`
+  width: 100%;
+`;
+
+const FormRow = styled.View`
+  padding-left: 10;
+  padding-right: 10;
+  padding-bottom: 20;
+`;
+
+const Input = styled.View`
+  padding-left: 10;
+  padding-right: 10;
+  padding-top: 10;
+  padding-bottom: 10;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+`;
+
+const StyledIcon = styled.View`
+  width: 15%;
+  align-items: center;
+`;
+
 const StyledInputText = styled.TextInput`
-  border-width: 1;
-  border-color: #000;
+backgroundColor: #ffff
+  padding-left: 15
+  border-width: 0.5;
+  border-color: #a9a9a9;
   border-radius: 30;
   flex: 1;
 `;
-const StyledInputText2 = styled.TextInput`
-  border-width: 1;
-  border-color: #000;
-  border-radius: 30;
-  flex: 4;
+
+const StyledTextTouchableOpacity = styled.TouchableOpacity`
+  align-items: flex-end;
+  padding-right: 10;
+  height: 30px;
 `;
 
-const StyledContainer = styled.View`
-  flex-direction: row
-  padding-left: 10;
-  padding-right: 10;
-  padding-bottom: 10;
+const StyledText = styled.Text``;
+//textdecorationline: underline;
+
+const StyledTouchableOpacity = styled.TouchableOpacity`
+  background: #1abef2;
+  width: 50%;
+  height: 40;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+  margin-top: 5;
+  margin-bottom: 5;
+  border-radius: 30;
 `;
 
 export default Login;
