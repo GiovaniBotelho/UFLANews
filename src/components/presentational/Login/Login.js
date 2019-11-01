0c00a09a00298a9e571803f0f79bab01ffef049e
Mauricio Vieira <mauriciomvdr@hotmail.com>
2019-10-31 23:50:07 -0300

    Tela de login finalizada

--- src/components/presentational/Login/Login.js
+++ src/components/presentational/Login/Login.js
@@ -1,69 +1,77 @@
 import React, {useState} from 'react';
-import {Text, Image} from 'react-native';
+import {Image} from 'react-native';
 import styled from 'styled-components';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import LinearGradient from 'react-native-linear-gradient';
 
 /* Images */
 import Logo from '../../../assets/header-login.png';
 import Mapa from '../../../assets/mapView.jpeg';
 
+/* Components - import */
+import Button from '../../config/Button';
+import TextInput from '../../config/TextInput';
+import TouchableTextWithIcon from '../../config/TouchableTextWithIcon';
+
 const Login = props => {
-  const [nome, setNome] = useState('');
+  const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [loading, setLoading] = useState(true);
 
-  console.log(props);
   handleLogin = () => {
+    // Aqui manipulamos o login
     setLoading(!loading);
-
-    // props.navigation.navigate('Register');
-  };
-
-  handleChangeText = nome => {
-    setNome(nome);
   };
 
   return (
     <>
-      <FormContainer colors={['#93cae8', '#c4cccf']}>
+      <StyledImages>
         <Image
-          style={{width: '100%', height: '50px', paddingTop: 0, marginTop: 0}}
           source={Mapa}
-          resizeMode={'contain'}
+          style={{height: 70, width: '100%'}}
+          resizeMode={'cover'}
         />
-        <Image style={{width: '100%'}} source={Logo} resizeMode={'contain'} />
-        <StyledView>
-          <FormRow>
-            <Input>
-              <StyledIcon>
-                <Icon name="envelope" size={25} color="#000000" />
-              </StyledIcon>
-              <StyledInputText
-                placeholder={'E-mail'}
-                value={nome}
-                onChangeText={texto => handleChangeText(texto)}
-              />
-            </Input>
-            <Input>
-              <StyledIcon>
-                <Icon name="lock" size={25} color="#000000" />
-              </StyledIcon>
-              <StyledInputText placeholder={'Senha'} />
-            </Input>
-          </FormRow>
-          <StyledTextTouchableOpacity>
-            <StyledText textDecorationLine={'underline'}>
-              Esqueci a senha
-            </StyledText>
-          </StyledTextTouchableOpacity>
-          <StyledTouchableOpacity>
-            <Text>Login</Text>
-          </StyledTouchableOpacity>
-          <StyledTouchableOpacity>
-            <Text>Cadastrar</Text>
-          </StyledTouchableOpacity>
-        </StyledView>
+        <Image
+          source={Logo}
+          style={{width: '100%', height: 120}}
+          resizeMode={'stretch'}
+        />
+      </StyledImages>
+      <FormContainer colors={['#e4ecee', '#93cae8']}>
+        <FormRow>
+          <TextInput
+            iconName={'envelope'}
+            iconSize={25}
+            iconColor={'#000'}
+            placeholder={'E-mail'}
+            value={name}
+            onChangeText={setName}
+          />
+          <TextInput
+            iconName={'lock'}
+            iconSize={25}
+            iconColor={'#000'}
+            placeholder={'Password'}
+            value={password}
+            onChangeText={setPassword}
+          />
+          <TouchableTextWithIcon text={'Esqueci a senha'} />
+        </FormRow>
+        <StyledButtonContainer>
+          <Button title={'ENTRAR'} onClick={handleLogin} />
+          <Button
+            title={'CADASTRAR'}
+            onClick={() => {
+              console.log('Cliquei');
+              props.navigation.navigate('Register');
+            }}
+          />
+        </StyledButtonContainer>
+
+        <TouchableTextWithIcon
+          iconName={'user'}
+          iconSize={20}
+          text={'Minha conta'}
+        />
       </FormContainer>
     </>
   );
@@ -72,65 +80,24 @@ const Login = props => {
 /* Componentes estilizados aqui */
 const FormContainer = styled(LinearGradient)`
   flex: 1;
-  justify-content: center;
 `;
 const StyledImages = styled.View`
   width: 100%;
   align-self: flex-start;
 `;
-const StyledView = styled.View`
-  width: 100%;
-`;
 
 const FormRow = styled.View`
+  padding-top: 80;
   padding-left: 10;
   padding-right: 10;
   padding-bottom: 20;
+  height: 50%;
 `;
 
-const Input = styled.View`
-  padding-left: 10;
-  padding-right: 10;
-  padding-top: 10;
-  padding-bottom: 10;
-  flex-direction: row;
+const StyledButtonContainer = styled.View`
   align-items: center;
   justify-content: space-around;
-`;
-
-const StyledIcon = styled.View`
-  width: 15%;
-  align-items: center;
-`;
-
-const StyledInputText = styled.TextInput`
-backgroundColor: #ffff
-  padding-left: 15
-  border-width: 0.5;
-  border-color: #a9a9a9;
-  border-radius: 30;
-  flex: 1;
-`;
-
-const StyledTextTouchableOpacity = styled.TouchableOpacity`
-  align-items: flex-end;
-  padding-right: 10;
-  height: 30px;
-`;
-
-const StyledText = styled.Text``;
-//textdecorationline: underline;
-
-const StyledTouchableOpacity = styled.TouchableOpacity`
-  background: #1abef2;
-  width: 50%;
-  height: 40;
-  align-items: center;
-  justify-content: center;
-  align-self: center;
-  margin-top: 5;
-  margin-bottom: 5;
-  border-radius: 30;
+  height: 40%;
 `;
 
 export default Login;
