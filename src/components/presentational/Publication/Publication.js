abd32971c74c9d77a561fbf51e86ecca56f18119
Mauricio Vieira <mauriciomvdr@hotmail.com>
2019-11-06 19:25:32 -0300

    :fire: Tela da publicação finalizada

--- src/components/presentational/Publication/Publication.js
+++ src/components/presentational/Publication/Publication.js
@@ -52,6 +52,24 @@ const Publication = props => {
             page editors now use Lorem Ipsum as their default model text, and a
             search for 'lorem ipsum' will uncover many web sites still in their
             infancy. Various versions have evolved over the years, sometimes by
+            accident, sometimes on purpose (injected humour and the like). Lorem
+            Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since
+            the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum. It is a long
+            established fact that a reader will be distracted by the readable
+            content of a page when looking at its layout. The point of using
+            Lorem Ipsum is that it has a more-or-less normal distribution of
+            letters, as opposed to using 'Content here, content here', making it
+            look like readable English. Many desktop publishing packages and web
+            page editors now use Lorem Ipsum as their default model text, and a
+            search for 'lorem ipsum' will uncover many web sites still in their
+            infancy. Various versions have evolved over the years, sometimes by
             accident, sometimes on purpose (injected humour and the like).
           </PublicationText>
         </Content>
@@ -88,6 +106,10 @@ const FullPublication = styled.ScrollView`
   margin-left: ${SPACING.medium};
   margin-bottom: ${SPACING.medium};
   margin-right: ${SPACING.medium};
+  padding-top: ${SPACING.small};
+  padding-bottom: ${SPACING.small};
+  padding-left: ${SPACING.small};
+  padding-right: ${SPACING.small};
 `;
 
 const PublicationTitle = styled.Text`
@@ -96,6 +118,7 @@ const PublicationTitle = styled.Text`
   margin-left: ${SPACING.medium};
   margin-right: ${SPACING.medium};
   align-self: center;
+  font-weight: bold;
 `;
 
 const PublicationAuthor = styled.Text`
@@ -120,12 +143,15 @@ const Content = styled.View`
   margin-left: ${SPACING.large};
 `;
 
-const PublicationText = styled.Text``;
+const PublicationText = styled.Text`
+  text-align: justify;
+`;
 
 const Options = styled.View`
   flex-direction: row;
   justify-content: flex-end;
   margin-right: ${SPACING.medium};
+  margin-bottom: ${SPACING.medium};
 `;
 
 const Option = styled.TouchableOpacity`
